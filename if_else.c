f746546a0d0c14f29f1956652768f928e42e3aef
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-10-08 10:17:40 +0600

    added if_else condition

---
+++ if_else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+
+    if (tk >= 100)
+    {
+        printf("i want to eat burgers\n");
+    }
+    else
+    {
+        printf("i want to eat fries\n");
+    }
+
+    return 0;
+}
