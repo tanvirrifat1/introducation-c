b062dc4f9edb37b5ae3335b4bf005d48dbb0ddec
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-10-13 15:22:01 +0600

    added more

---
+++ wlc_with_con.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int A;
+    int B;
+
+    scanf("%d %d", &A, &B);
+
+    if (A >= B)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}
