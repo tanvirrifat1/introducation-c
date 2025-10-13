c2b2e9586307d8c701de64a160c5afee781d111b
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-10-13 16:12:34 +0600

    added #include <stdio.h>
    int main()
    {
        int A;
        int B;
    
        scanf("%d %d", &A, &B);
    
        if (A % B == 0)
        {
            printf("Multiples\n");
        }
        else if (B % A == 0)
        {
            printf("Multiples\n");
        }
        else
        {
            printf("No Multiples\n");
        }
        return 0;
    }

--- wlc_with_con.c
+++ wlc_with_con.c
@@ -2,20 +2,22 @@
 
 int main()
 {
-
     int A;
     int B;
 
     scanf("%d %d", &A, &B);
 
-    if (A >= B)
+    if (A % B == 0)
     {
-        printf("Yes\n");
+        printf("Multiples\n");
+    }
+    else if (B % A == 0)
+    {
+        printf("Multiples\n");
     }
     else
     {
-        printf("No");
+        printf("No Multiples\n");
     }
-
     return 0;
 }
