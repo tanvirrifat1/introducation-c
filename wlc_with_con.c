c4f67b540d899fbfeaa8b38c171335ead3fd510e
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-10-15 16:26:18 +0600

    added more

--- wlc_with_con.c
+++ wlc_with_con.c
@@ -3,35 +3,5 @@
 int main()
 {
 
-    int a, b, c;
-
-    scanf("%d %d %d", &a, &b, &c);
-
-    if (a <= b && a <= c)
-    {
-        printf("%d ", a);
-    }
-    else if (b <= a && b <= c)
-    {
-        printf("%d ", b);
-    }
-    else if (c <= a && c <= b)
-    {
-        printf("%d ", c);
-    }
-
-    if (a >= b && a >= c)
-    {
-        printf("%d ", a);
-    }
-    else if (b >= a && b >= c)
-    {
-        printf("%d ", b);
-    }
-    else if (c >= a && c >= b)
-    {
-        printf("%d ", c);
-    }
-
-    return 0;
+       return 0;
 }
