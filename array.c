c63008cc1334e1a0bddcbb8c65f06ead9e6c7ef8
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-10-17 12:45:12 +0600

    added array

--- array.c
+++ array.c
@@ -3,11 +3,25 @@
 int main()
 {
 
-    int a[5] = {0};
+    int N;
 
-    for (int i = 0; i < 5; i++)
+    scanf("%d", &N);
+
+    int A[N];
+
+    for (int i = 0; i < N; i++)
     {
-        printf("%d ", a[i]);
+        scanf("%d", &A[i]);
     }
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+
+        if (i % 2 != 0)
+        {
+            printf("%d ", A[i]);
+        }
+    }
+
     return 0;
 }
