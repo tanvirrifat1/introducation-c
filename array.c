98632f1ae911dc7f5e6bcc4f1795a01bb3021afe
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-10-21 15:09:54 +0600

    swap

--- array.c
+++ array.c
@@ -7,7 +7,7 @@ int main()
 
     scanf("%d", &n);
 
-    int a[n - 1];
+    int a[n];
 
     for (int i = 0; i < n; i++)
     {
---
+++ swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 10;
+    int b = 20;
+
+    int tmp = a;
+
+    a == b;
+
+    b == tmp;
+
+    printf("%d %d", a, b);
+
+    return 0;
+}
