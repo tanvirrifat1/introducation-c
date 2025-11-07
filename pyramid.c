2f28ebd8da2c6b571f5cc12d31cc5bb450ec9517
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-11-07 15:01:29 +0600

    added

--- pyramid.c
+++ pyramid.c
@@ -1,10 +1,10 @@
+
+
 #include <stdio.h>
 
 int main()
 {
-
     int n;
-
     scanf("%d", &n);
 
     int a[n];
@@ -15,14 +15,25 @@ int main()
     }
 
     int x;
+    scanf("%d", &x);
+
+    int flag = 0;
 
     for (int i = 0; i < n - 1; i++)
     {
-
         for (int j = i + 1; j < n; j++)
         {
-            printf("%d %d\n", a[i], a[j]);
+            if (a[i] + a[j] == x)
+            {
+                flag = 1;
+                printf("Yes\n");
+            }
         }
+    }
+
+    if (flag == 0)
+    {
+        printf("No\n");
     }
 
     return 0;
