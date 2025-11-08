74ab12211c5082a1e316895ffa0a77e762b78340
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-11-08 17:03:50 +0600

    added

--- selection_sort.c
+++ selection_sort.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int sum(int sum1, int sum2)
+int sum()
 {
 
-    int ans = sum1 + sum2;
-    return ans;
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    int val = a + b;
+
+    printf("%d", val);
 }
 
 int main()
 
 {
 
-    int val = sum(10, 60);
-    int val2 = sum(-20, 45);
-
-    printf("%d %d", val, val2);
+    sum();
 
     return 0;
 }
