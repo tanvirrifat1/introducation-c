7b65d5091a137810a741edeabc498486b61e0188
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-11-09 17:10:44 +0600

    added more

--- selection_sort.c
+++ selection_sort.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
-int fun(int a[])
-{
-
-    for (int i = 0; i < 5; i++)
-    {
-
-        printf("%d\n", a[i]);
-    }
-}
-
 int main()
 {
 
-    int a[5] = {1, 5, 6, 8, 9};
+    int N;
 
-    fun(a);
+    scanf("%d", &N);
+
+    int space = N - 1;
+
+    int val = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= val; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+        val++;
+        space--;
+    }
 
     return 0;
 }
