6bc8565fa79798d9b4beda3c81a71593d010d10c
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-11-09 15:44:20 +0600

    added

--- selection_sort.c
+++ selection_sort.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-int main()
+int fun(int a[])
 {
-
-    int a[5] = {1, 2, 3, 4, 5};
-
-    *a = 100;
 
     for (int i = 0; i < 5; i++)
     {
+
         printf("%d\n", a[i]);
     }
+}
+
+int main()
+{
+
+    int a[5] = {1, 5, 6, 8, 9};
+
+    fun(a);
 
     return 0;
 }
